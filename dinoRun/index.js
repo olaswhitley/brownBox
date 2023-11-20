2d1c274c7176312bc9e577306d6b0fad2c9000b4
Nick Whitley <olaswhitley@gmail.com>
2023-11-19 21:20:38 -0500

    Added animation to Dino Run

--- dinoRun/index.js
+++ dinoRun/index.js
@@ -1,20 +1,30 @@
 import Player from "./Player.js";
+import Ground from "./Ground.js";
 
 const canvas = document.getElementById("gameArea");
 const ctx = canvas.getContext("2d");
 
+const GAME_SPEED_START = 0.75; // 1.0
+const GAME_SPEED_INCREMENT = 0.00001;
+
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 200;
-const PLAYER_WIDTH = 88/ 1.5;
+const PLAYER_WIDTH = 88 / 1.5;
 const PLAYER_HEIGHT = 94 / 1.5;
 const MAX_JUMP_HEIGHT = GAME_HEIGHT;
 const MIN_JUMP_HEIGHT = 150;
+const GROUND_WIDTH = 2400;
+const GROUND_HEIGHT = 24;
+const GROUND_AND_CACTUS_SPEED = 0.5;
+
 
 // Game Objects
 let player = null;
+let ground = null;
 
 let scaleRatio = null;
 let previousTime = null;
+let gameSpeed = GAME_SPEED_START;
 
 function createSprites() {
     const playerWidthInGame = PLAYER_WIDTH * scaleRatio;
@@ -22,7 +32,11 @@ function createSprites() {
     const minJumpHeightInGame = MIN_JUMP_HEIGHT * scaleRatio;
     const maxJumpHeightInGame = MAX_JUMP_HEIGHT * scaleRatio;
 
+    const groundWidthInGame = GROUND_WIDTH * scaleRatio;
+    const groundHeightInGame = GROUND_HEIGHT * scaleRatio;
+
     player = new Player(ctx, playerWidthInGame, playerHeightInGame, minJumpHeightInGame, maxJumpHeightInGame, scaleRatio);
+    ground = new Ground(ctx, groundWidthInGame, groundHeightInGame, GROUND_AND_CACTUS_SPEED, scaleRatio);
 }
 
 function setScreen() {
@@ -44,7 +58,7 @@ function getScaleRatio() {
     const screenHeight = Math.min(window.innerHeight, document.documentElement.clientHeight);
     const screenWidth = Math.min(window.innerWidth, document.documentElement.clientWidth);
 
-    //window is wider than the game width
+    // Window is wider than the game width
     if(screenWidth / screenHeight < GAME_WIDTH / GAME_HEIGHT) {
         return screenWidth / GAME_WIDTH;
     }
@@ -71,8 +85,11 @@ function gameLoop(currentTime) {
     clearScreen();
 
     // Update game objects
+    ground.update(gameSpeed, frameTimeDelta);
+    player.update(gameSpeed, frameTimeDelta);
 
     // Draw game objects
+    ground.draw();
     player.draw();
     requestAnimationFrame(gameLoop);
 }
